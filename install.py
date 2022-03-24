8fc2de06096a8f099d48e58d26710643b4941229
Martin Olivier <martin.olivier@live.fr>
2022-03-24 20:46:05 +0100

    fix: CI

--- install.py
+++ install.py
@@ -13,13 +13,13 @@ if os.system('cmake --build build') != 0:
   exit(1)
 
 actual_os = platform.system()
-bin = ""
-install_path = ""
 
 if actual_os == "Darwin":
+  tool = "cp"
   bin = "deus"
   install_path = "/usr/local/bin"
 elif actual_os == "Linux":
+  tool = "cp"
   bin = "deus"
   usr = os.getenv("USERNAME")
   if usr == None:
@@ -27,13 +27,14 @@ elif actual_os == "Linux":
   else:
     install_path = "/home/" + usr + "/.local/bin"
 elif actual_os == "Windows":
+  tool = "copy"
   bin = "Debug/deus.exe"
   install_path = "$env:UserProfile/AppData/Local/Microsoft/WindowsApps"
 else:
   print("Unknown system, you will need to install Deus manually", file=sys.stderr)
   exit(1)
 
-if os.system("cp " + bin + " " + install_path) != 0:
+if os.system(tool + " " + bin + " " + install_path) != 0:
   print("Failed to install Deus at \"" + install_path + "\"", file=sys.stderr)
   exit(1)
 
